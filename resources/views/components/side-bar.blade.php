0d25cf4f19b466719d121a4ff825c11701b067fc
soleilsolis <fukuman23@gmail.com>
2022-11-20 10:27:41 +0800

    sdf

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -1,8 +1,18 @@
-<div class="ui big blue inverted visible borderless vertical sidebar menu portrait:hidden">
+@php
+     use Illuminate\Support\Facades\Auth;
+
+@endphp
+
+<div class="ui big   @if (Auth::user()->type === 'admin') inverted black @elseif(Auth::user()->type === 'patient') inverted blue    @endif  visible borderless vertical sidebar menu portrait:hidden">
     <div class="item flex items-center">
         <img src="{{ Vite::asset('resources/image/logo.png') }}" alt="logo" class="ui circular w-16 image inline">
         <span class="ml-6 text-4xl font-semibold">
             RM Dental
+            <span class="text-lg font-normal">
+                @if (Auth::user()->type != 'patient')
+                    {{ ucfirst(Auth::user()->type) }}
+                @endif
+            </span>
         </span>
     </div>
 
@@ -18,7 +28,7 @@
         </span>
     </a>
 
-    @if (\Illuminate\Support\Facades\Auth::user()->type === 'admin')
+    @if (Auth::user()->type === 'admin')
         <a href="/services" class="item ">
             <span class="font-medium text-2xl">
                 Services
